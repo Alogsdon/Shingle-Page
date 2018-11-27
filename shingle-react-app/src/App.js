df1ac865227efc1731988c0911271815797dc460
Andrew Logsdon <logsdon.85@buckeyemail.osu.edu>
2018-11-27 09:33:49 -0500

    product select, display image render

--- shingle-react-app/src/App.js
+++ shingle-react-app/src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const SHINGLES = require('./shingles.json');
+const PrevNextEnum = Object.freeze({ "prev": 1, "next": 2 })
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: SHINGLES[0].uid
+    };
+  }
+
+  handleProdSelect(event) {
+    this.setState({
+      product: event.target.value
+    });
+  }
+
+  handleArrowClick(x) {
+    console.log(x);
+  }
+
   render() {
+    const productOptions = SHINGLES.map(function (prod) {
+      return (
+        <option value={prod.uid} key={prod.uid}>
+          {prod.name}
+        </option>
+      );
+    });
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="display-image">
+          <button id="prev-color-button" onClick={() => this.handleArrowClick(PrevNextEnum.prev)}>
+            &#9664;
+          </button>
+          <div id="display-image-area"></div>
+          <button id="next-color-button" onClick={() => this.handleArrowClick(PrevNextEnum.next)}>
+            &#9654;
+          </button>
+        </div>
+        <div className="product-select">
+          <label>
+            Shingle Line:
+            <select value={this.state.product} onChange={e => this.handleProdSelect(e)}>
+              {productOptions}
+            </select>
+          </label>
+        </div>
       </div>
     );
   }
